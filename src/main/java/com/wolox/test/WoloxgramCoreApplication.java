09a79dafb1c4ddf0fcf3c6ab78c9ec00f294a767
richard <richard.marrugo@itxpertix.com>
2021-04-16 14:37:30 -0500

    first commit

---
+++ src/main/java/com/wolox/test/WoloxgramCoreApplication.java
@@ -0,0 +1,13 @@
+package com.wolox.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoloxgramCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoloxgramCoreApplication.class, args);
+	}
+
+}
