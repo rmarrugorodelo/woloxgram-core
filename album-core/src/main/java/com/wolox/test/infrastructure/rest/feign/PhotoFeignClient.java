189ccf1f2a6cfbebd64f6ca904a1201df88636e3
richard <richard.marrugo@itxpertix.com>
2021-04-17 16:35:52 -0500

    Se ajusta servicios de buscar photos y albunes por usuario

--- album-core/src/main/java/com/wolox/test/infrastructure/rest/feign/PhotoFeignClient.java
+++ album-core/src/main/java/com/wolox/test/infrastructure/rest/feign/PhotoFeignClient.java
@@ -3,7 +3,6 @@ package com.wolox.test.infrastructure.rest.feign;
 import com.wolox.test.infrastructure.rest.feign.response.PhotoRestResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -14,8 +13,5 @@ public interface PhotoFeignClient {
 
     @GetMapping(value = "${feign.jsonplaceholder.photos}", produces = APPLICATION_JSON_VALUE)
     List<PhotoRestResponse> findAll();
-
-    @GetMapping(value = "${feign.jsonplaceholder.users-photos}", produces = APPLICATION_JSON_VALUE)
-    List<PhotoRestResponse> findByUser(@PathVariable("userId") Long userId);
 
 }
